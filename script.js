e32d980056a77259050756ae4e4f1ca8044edec3
Majd <mrmea01@gmail.com>
2026-03-23 21:20:09 +0100

    added interactivity with javascript

---
+++ script.js
@@ -0,0 +1,11 @@
+function openProject(project) {
+
+    if (project === "portfolio") {
+        alert("Das ist meine erste Portfolio Website.");
+    }
+
+    if (project === "task") {
+        alert("Task Manager Projekt kommt bald.");
+    }
+
+}
