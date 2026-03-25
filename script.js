848049f3e7d886aab384ac9d3fa207418cd767db
Majd <mrmea01@gmail.com>
2026-03-25 01:04:17 +0100

    UI Upgrade

--- script.js
+++ script.js
@@ -1,11 +1,42 @@
-function openProject(project) {
+// Scroll to contact
+function scrollToContact() {
+    document.getElementById("contact").scrollIntoView({
+        behavior: "smooth"
+    });
 
-    if (project === "portfolio") {
-        alert("Das ist meine erste Portfolio Website.");
-    }
-
-    if (project === "task") {
-        alert("Task Manager Projekt kommt bald.");
-    }
-
+    setTimeout(() => {
+        document.getElementById("name").focus();
+    }, 500);
 }
+
+// Scroll animation
+function reveal() {
+    let elements = document.querySelectorAll(".fade-in");
+
+    elements.forEach(el => {
+        let top = el.getBoundingClientRect().top;
+        let windowHeight = window.innerHeight;
+
+        if (top < windowHeight - 50) {
+            el.classList.add("show");
+        }
+    });
+}
+
+window.addEventListener("scroll", reveal);
+
+// Dummy request (Frontend)
+function sendRequest() {
+    let name = document.getElementById("name").value;
+    let message = document.getElementById("message").value;
+
+    if (name === "" || message === "") {
+        alert("Bitte alles ausfüllen");
+        return;
+    }
+
+    alert("Anfrage gesendet!");
+}
+document.addEventListener("mousemove", (e) => {
+    document.body.style.backgroundPosition = `${e.clientX/50}px ${e.clientY/50}px`;
+});
